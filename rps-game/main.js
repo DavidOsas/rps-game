610776eefa30845def18742db23ae0983eaf340c
DavidOsas <oyairod@gmail.com>
2023-11-11 18:00:25 +0100

    Code works now. Will make improvements later

--- rps-game/main.js
+++ rps-game/main.js
@@ -1,97 +1,101 @@
-let turns = 0
+/* Declare global variables */
+
+let turns = 6
+
+let playerScore = 0
+let computerScore = 0
 
 /* Conditional to loop code 5 times */
 
-while (turns < 5) {
-	    
-	    /* Main function to wrap everything */
-	    
-	    function game() {
-		            let choices = ["rock", "paper", "scissors"]
-		            
-		            /* Function to return the computer's choice */
-		            
-		            function getComputerChoice() {
-				              
-				                let number = Math.floor(Math.random() * 3)
-				                
-				                let computerChoice = choices[number]
-				                
-				                return computerChoice
-				            }
-		            
-		            getComputerChoice()
-		            
-		            /* Function to return the player's choice */
-		            
-		            let playRound = (playerSelection, computerSelection) => {
-				                
-				                let playerPrompt = prompt("Please enter your choice: ")
-				                
-				                let playerChoice = playerPrompt.toLowerCase()
-				                
-				                if( playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
-							                
-							                turns++
-							                return playerChoice
-							                
-							            } else {
-									                    
-									                    alert("Wrong input!!!")
-									                    
-									                }
-				            }
-		            
-		            playRound()
-		            
-		            /* Accessing the returned values outside their function scope */
-		            
-		            let playerResult = playRound()
-		            let computerResult = getComputerChoice()
-		            
-		            /* Creating variables to keep score */
-		            
-		            let playerScore = 0
-		            let computerScore = 0
-		            
-		            /* Conditional to keep track of score and determine the winner */
-		            
-		            if ( choices.indexOf(computerResult) == 0 && choices.indexOf(playerResult) == 2 ) {
-				                
-				                playerScore++
-				                
-				            } else if ( choices.indexOf(playerResult) == 0 && choices.indexOf(computerResult) == 2 ) {
-						                
-						                playerScore++
-						                
-						               } else if ( choices.indexOf(playerResult) > choices.indexOf(computerResult) ){
-								                      
-								                      playerScore++
-								                      
-								                  }
-		               
-		               else if ( choices.indexOf(playerResult) < choices.indexOf(computerResult) ) {
-				                      
-				                      computerScore++
-				                      
-				                  } else if ( choices.indexOf(playerResult) == choices.indexOf(computerResult) ) {
-							                 
-							                 return
-							                 
-							             }
-		            
-		            /* Logging returned values to the console */
-		            
-		            console.log(`You played ${playRound()} and your current score is ${playerScore}, while the computer played ${getComputerChoice()}, and it has a score of ${computerScore}`)
-		            
-		        }
-	    
-	    /* Calling wrapper function "game()" */
-	    
-	    game()
-	    
-	    /* Termination condition for loop */
-	    
-	    turns++
-	    
+while (turns > 1) {
+
+    /* Wrapper function "game()" */
+
+    function game() {
+
+        /* Function to return computer selection */
+
+        function computerChoice() {
+
+            let choices = ["rock", "paper", "scissors"]
+
+            let numero = Math.floor(Math.random() * 3)
+
+            let computerResult = choices[numero]
+
+            return computerResult
+        }
+        
+        /* Function to return player selection */
+
+        function playerChoice() {
+
+            let playerInput = prompt("Please enter your choice: ")
+
+            let playerResult = playerInput.toLowerCase()
+
+            if (playerResult === "rock" || playerResult === "paper" || playerResult === "scissors") {
+
+                return playerResult
+
+            } else {
+
+                throw new Error("Wrong input!")
+
+                return
+
+            }
+
+        }
+        
+        /* Accessing function variables in a higher scope */
+
+        let player = playerChoice()
+        let computer = computerChoice()
+        
+        /* Logic to decide the winner of a round, and determine the score */
+        
+        if (player === computer) {
+
+            console.log(`It's a tie!`)
+
+        } else if (player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" & computer === "paper") {
+
+            playerScore++
+
+            console.log(`You win! Your ${player} beats the computer's ${computer}, and your score is ${playerScore}`)
+
+
+        } else {
+
+            computerScore++
+
+            console.log(`You lose! The computer's ${computer} beats your ${player}, and the computer has won ${computerScore} rounds.`)
+
+        }
+
+
+    }
+
+    game()
+    
+    /* Loop termination statement */
+    
+    turns--
+
+}
+
+/* Code to output the overall winner after 5 rounds */
+
+if (turns == 1 && playerScore > computerScore) {
+
+    console.log(`GAME OVER! YOU WIN! THE SCORE IS: ${playerScore} — ${computerScore}`)
+} else if (computerScore === playerScore) {
+
+    console.log(`GAME OVER! IT'S A TIE!`)
+
+} else {
+
+    console.log(`GAME OVER! YOU LOSE! THE SCORE IS: ${playerScore} — ${computerScore}`)
+
 }
